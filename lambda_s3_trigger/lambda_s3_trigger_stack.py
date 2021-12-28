ff5ab6ac13082c7bb0a62bdb6ebf14cfdf31a31b
txdasari <txdasari@stuller.com>
2021-12-28 09:40:20 -0600

    initial commit

--- lambda_s3_trigger/lambda_s3_trigger_stack.py
+++ lambda_s3_trigger/lambda_s3_trigger_stack.py
@@ -1,6 +1,10 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    Tags,
+    aws_lambda as _lambda,
+    aws_s3 as s3,
+    aws_s3_notifications,
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -10,10 +14,26 @@ class LambdaS3TriggerStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+          # create lambda function
+        function = _lambda.Function(self, "lambda_function",
+                                    runtime=_lambda.Runtime.PYTHON_3_9,
+                                    handler="lambda-handler.main",
+                                    code=_lambda.Code.from_asset("./lambda"))
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "LambdaS3TriggerQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+
+        # create s3 bucket
+        bucket = s3.Bucket(self, "securityhubBucket")
+     
+        Tags.of(self).add('Application', 'Test')
+        Tags.of(self).add('Environment', 'Dev')
+        Tags.of(self).add('Name', 'TJTest')
+        Tags.of(self).add('Team', 'CloudOpsDev')
+
+        # create s3 notification for lambda function
+        notification = aws_s3_notifications.LambdaDestination(function)
+
+        # assign notification for the s3 event type (ex: OBJECT_CREATED)
+        bucket.add_event_notification(s3.EventType.OBJECT_CREATED, notification)
+    
+
+       
